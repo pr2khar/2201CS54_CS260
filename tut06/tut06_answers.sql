d07079ae9492c5fb421dc307db5bcf31bcbc65d3
pr2khar <153148208+pr2khar@users.noreply.github.com>
2024-04-15 23:56:17 +0530

    Update tut06_answers.sql

--- tut06/tut06_answers.sql
+++ tut06/tut06_answers.sql
@@ -2,38 +2,11 @@
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
 
-create database tut6;
+-- PRAKHAR SHUKLA
+-- Roll Number : 2201CS54
+
 use tut6;
 
-CREATE TABLE students (
-    student_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    age INT,
-    city VARCHAR(50)
-);
-
-CREATE TABLE instructors (
-    instructor_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50)
-);
-
-CREATE TABLE courses (
-    course_id INT PRIMARY KEY,
-    course_name VARCHAR(100),
-    instructor_id INT,
-    FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id)
-);
-
-CREATE TABLE enrollments (
-    enrollment_id INT PRIMARY KEY,
-    student_id INT,
-    course_id INT,
-    grade VARCHAR(10),
-    FOREIGN KEY (student_id) REFERENCES students(student_id),
-    FOREIGN KEY (course_id) REFERENCES courses(course_id)
-);
 
 -- 1)
 select s.first_name, s.last_name, c.course_name
