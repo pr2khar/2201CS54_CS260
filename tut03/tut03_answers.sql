9f2cb3b17354ac0355092d557c99a38ced88f8c5
pr2khar <153148208+pr2khar@users.noreply.github.com>
2024-04-15 23:49:06 +0530

    Update tut03_answers.sql - Removed Create Table instructions

--- tut03/tut03_answers.sql
+++ tut03/tut03_answers.sql
@@ -5,39 +5,8 @@
 create database tut3;
 use tut3;
 
-CREATE TABLE students (
-    student_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    age INT,
-    city VARCHAR(50),
-    state VARCHAR(50)
-);
-
-CREATE TABLE instructors (
-    instructor_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(100)
-);
-
-CREATE TABLE courses (
-    course_id INT PRIMARY KEY,
-    course_name VARCHAR(100),
-    credit_hours INT,
-    instructor_id INT,
-    FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id)
-);
-
-CREATE TABLE enrollments (
-    enrollment_id INT PRIMARY KEY,
-    student_id INT,
-    course_id INT,
-    enrollment_date DATE,
-    grade VARCHAR(10),
-    FOREIGN KEY (student_id) REFERENCES students(student_id),
-    FOREIGN KEY (course_id) REFERENCES courses(course_id)
-);
+-- PRAKHAR SHUKLA
+-- Roll Number : 2201CS54
 
 -- 1)
 select first_name, last_name
