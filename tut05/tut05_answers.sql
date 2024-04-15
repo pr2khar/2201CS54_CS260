da6d729a45e0609a47a6384848120a8effb9240c
pr2khar <153148208+pr2khar@users.noreply.github.com>
2024-04-15 23:52:48 +0530

    Update tut05_answers.sql

--- tut05/tut05_answers.sql
+++ tut05/tut05_answers.sql
@@ -1,7 +1,10 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
-create database tut5;
+
+-- PRAKHAR SHUKLA
+-- Roll Number : 2201CS54
+
 use tut5;
 
 CREATE TABLE departments (
